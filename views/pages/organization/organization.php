bda22a27d231961fa633ecca8d1a00e75865b748
Alejandro Hernandez <alexhdzsolis@hotmail.com>
2021-06-11 07:04:35 -0500

    Se comenzó la página Organization

---
+++ views/pages/organization/organization.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+      //Include Head
+      include_once("../ButterflyProtocol/views/templates/head.php");
+    ?>
+    <link rel="stylesheet" href="../ButterflyProtocol/views/pages/organization/organization.css">
+
+    <title>Butterfly Protocol :: Home</title>
+  </head>
+  <body>
+
+    <div class="container-fluid">
+      
+    </div>
+
+    <div class="main_container">
+      <?php
+        //Include Main Header
+        include_once("../ButterflyProtocol/views/templates/header.php");
+      ?>  
+    
+      <div class="section_2">
+        <p>The Butterfly Protocol is intended to be a decentralized autonomous organization (DAO) that focuses on bringing the most robust naming environment to the decentralized internet. Butterfly was built to allow anyone in the world who has an internet connection to access and create content and applications without fear of censorship. Its leaders guide more than they dictate the future of what Butterfly can be. With a combination of proprietary and open-source tools and platforms, Butterfly allows anyone to participate by sponsoring top-level domains, creating dapps and more.</p>
+      </div>
+    
+      <div class="section_3">
+        <h2>Guided by</h2>
+        
+        <div class="container_profiles">
+          <div class="card_profile">
+            <img src="../ButterflyProtocol/assets/img/Dana-Farbo.jpeg" alt="Dana Forbo - President">
+            <h3>DANA FARBO</h3>
+            <p>President</p>
+            <i class="fs fa-linkedin "></i>
+          </div>
+  
+          <div class="card_profile">
+            <img src="../ButterflyProtocol/assets/img/Eddie-Quiroz.jpeg" alt="Eddie Quiroz - Decentralized Application Expansion">
+            <h3>EDDIE QUIROZ</h3>
+            <p>Decentralized Application Expansion</p>
+            <i class="fs fa-linkedin "></i>
+          </div>
+  
+          <div class="card_profile">
+            <img src="../ButterflyProtocol/assets/img/Josh-Robinson.jpeg" alt="Josh Robinson - Technology Vision and Architecture">
+            <h3>JOSH ROBINSON</h3>
+            <p>Technology Vision and Architecture</p>
+            <i class="fs fa-linkedin "></i>
+          </div>
+  
+          <div class="card_profile">
+            <img src="../ButterflyProtocol/assets/img/Jacobo-Castellanos.jpg" alt="Jacobo Castellanos - Common Interface Design">
+            <h3>JACOBO CASTELLANOS</h3>
+            <p>Common Interface Design</p>
+            <i class="fs fa-linkedin "></i>
+          </div>
+  
+          <div class="card_profile">
+            <img src="../ButterflyProtocol/assets/img/Alejandro-Hernandez.jpeg" alt="Alejandro Hernández - Developer">
+            <h3>ALEJANDRO HERNÁNDEZ</h3>
+            <p>Developer</p>
+            <i class="fs fa-linkedin "></i>
+          </div>
+        </div>
+      </div>
+
+      <div class="section_4">
+        <h4>Butterfly is more than a replacement. It’s an upgrade.</h4>
+        <hr>
+        
+        <div class="container_boxes">
+          
+          <div>
+            <div>
+              <img src="./assets/img/icon_1.svg" alt="" srcset="">
+            </div>
+            <h6>Censorship Resistance</h6>
+            <p>Websites are constantly being blocked by hostile state actors. With Butterfly, your name lives on the blockchain forever and can’t be taken down.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_2.svg" alt="" srcset="">
+            </div>
+            <h6>Decentralized Control</h6>
+            <p>Butterfly takes control back from centralized authorities and gives it to the people who own the names.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_3.svg" alt="" srcset="">
+            </div>
+            <h6>True Ownership</h6>
+            <p>You found the perfect name for your site, so why should you pay every year just to keep it? Or get it taken away just because you pissed the wrong people off? With Butterfly, when you own a name, you own it forever.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_4.svg" alt="" srcset="">
+            </div>
+            <h6>Flexible Naming</h6>
+            <p>With the existing system, you are limited to simple ASCII characters. With Butterfly, you can use Chinese names like 商业 and even emojis.☺</p>
+          </div>
+          
+          <div>
+            <div>
+              <img src="./assets/img/icon_5.svg" alt="" srcset="">
+            </div>
+            <h6>Global Identification</h6>
+            <p>One name, many ways to connect. A single name can go to your website, used to send you a secure message, or link to a crypto wallet.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_6.svg" alt="" srcset="">
+            </div>
+            <h6>Profitable</h6>
+            <p>Not only can you sell names you own through the built-in auction site, but you can sell sub names too. With Butterfly, making money from online properties is no longer the domain of squatters.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_7.svg" alt="" srcset="">
+            </div>
+            <h6>Future Proof</h6>
+            <p>Everything in Butterfly can be upgraded from how the record is stored to where it points.</p>
+          </div>
+  
+          <div>
+            <div>
+              <img src="./assets/img/icon_8.svg" alt="" srcset="">
+            </div>
+            <h6>Simple</h6>
+            <p>Install the simple browser extension and you are ready to access the new internet. There are no servers to install, and no need trying to figure out what the heck a C record is.</p>
+          </div>
+        </div>
+      
+      </div>
+    
+      <div class="section_5">
+        <div class="s5_c1">
+          <h2>Get Involved</h2>
+          <p>Subscribe to our mailing list or contact us through any of the following channels</p>
+        </div>
+        <div class="s5_c2">
+          <!-- Begin Constant Contact Inline Form Code -->
+          <div class="ctct-inline-form" data-form-id="a822e1e4-8dfe-453b-a057-55c3cc8f9dcf"></div>
+          <!-- End Constant Contact Inline Form Code -->
+        </div>
+      </div>
+
+    </div>
+    <!-- Begin Constant Contact Active Forms -->
+    <script> var _ctct_m = "a5b89f36f45290075e5533325d23a827"; </script>
+    <script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+    <!-- End Constant Contact Active Forms -->
+  </body>
+</html>
