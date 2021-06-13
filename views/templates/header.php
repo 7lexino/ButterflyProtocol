b4b1d88f0c17a2735a2e0e587adbe4f6fb013594
Ashizar <mariela.778@gmail.com>
2021-06-13 00:51:25 -0500

    token page + footer

--- views/templates/header.php
+++ views/templates/header.php
@@ -6,7 +6,7 @@
     <li class="fas fa-lg fa-bars"></li>
     </div>
     <div id="main_menu" class="layout_main_menu">
-    <nav id="ejm2">
+    <nav id="ani-navbar">
         <a href="#" class="menu_link">Organization</a>
         <a href="#" class="menu_link">Resources</a>
         <a href="#" class="menu_link">Blog</a>
@@ -14,7 +14,7 @@
         <a href="#" class="menu_link">Domain Dashboard</a>
         <a href="#" class="menu_link">NFT Marketplace</a>
     </nav>
-    <span id="ejm4">
+    <span>
         <li><a target="_blank" href="https://twitter.com/butterflyproto/"><img src="./assets/img/twitter_icon.svg" alt=""></a></li>
         <li><a target="_blank" href="https://medium.com/bproto"><img src="./assets/img/m_icon.svg" alt=""></a></li>
         <!-- <li><a target="_blank" href="mailto:sales@butterflyprotocol.io"><img width="25px" src="./assets/img/email_icon.svg" alt=""></a></li> -->
