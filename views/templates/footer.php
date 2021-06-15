9000bb333a5859ca7449872a199b2ff8c830360e
Alejandro Hernandez <alexhdzsolis@hotmail.com>
2021-06-15 10:58:54 -0500

    Nuevas imagenes en dashboard, link a la app, lightbox en botones, quitar nft en menu, arreglo de footer, personalizacion de constact conact, correcion de link en whitepaper, texto guided by en organization

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -5,18 +5,18 @@
   </div>
 
   <div class="s5_c2">
-    <span>
-      <li><a target="_blank" href="https://twitter.com/butterflyproto/"><img src="./assets/img/twitter_icon.svg" alt=""></a></li>
-      <li><a target="_blank" href="https://medium.com/bproto"><img src="./assets/img/m_icon.svg" alt=""></a></li>
-      <li><a target="_blank" href="https://t.me/bproto"><img width="25px" src="./assets/img/telegram_icon.svg" alt=""></a></li>
-      <li><a target="_blank" href="mailto:info@bproto.io"><img width="25px" src="./assets/img/email_icon.svg" alt=""></a></li>
+    <span style="margin-top:30px;margin-right:20px;">
+      <li><a target="_blank" href="https://twitter.com/butterflyproto/"><img width="50px" src="./assets/img/twitter_icon.svg" alt=""></a></li>
+      <li><a target="_blank" href="https://medium.com/bproto"><img width="50px" src="./assets/img/m_icon.svg" alt=""></a></li>
+      <li><a target="_blank" href="https://t.me/bproto"><img width="50px" src="./assets/img/telegram_icon.svg" alt=""></a></li>
+      <li><a target="_blank" href="mailto:info@bproto.io"><img width="50px" src="./assets/img/email_icon.svg" alt=""></a></li>
     </span>
 
     <div class="box-email">
-      <input type="email" placeholder="Email">
-      <button class="btn-submit">Submit</button>
+      <!-- <input type="email" placeholder="Email">
+      <button class="btn-submit">Submit</button> -->
       <!-- Begin Constant Contact Inline Form Code -->
-      <!-- <div class="ctct-inline-form" data-form-id="a822e1e4-8dfe-453b-a057-55c3cc8f9dcf"></div> -->
+      <div class="ctct-inline-form" data-form-id="a822e1e4-8dfe-453b-a057-55c3cc8f9dcf"></div>
       <!-- End Constant Contact Inline Form Code -->
     </div>
   </div>
